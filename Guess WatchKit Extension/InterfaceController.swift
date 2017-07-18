7057b74323324cdbb8552bfc9631c0bd496fc926
Tommy Tsui <tommytsui@graces-imac.local>
2017-07-18 16:12:46 +0800

    source: http://www.appcoda.com/watchkit-introduction-tutorial/

--- Guess WatchKit Extension/InterfaceController.swift
+++ Guess WatchKit Extension/InterfaceController.swift
@@ -11,6 +11,30 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    
+    
+    @IBOutlet var guessSlider: WKInterfaceSlider!    // the slider
+    @IBOutlet var guessLabel: WKInterfaceLabel!     // the label displaying the guess number
+    @IBOutlet var resultLabel: WKInterfaceLabel!    // Wrong/Correct Label
+
+    @IBAction func updateGuess(_ value: Float) {
+        guessNumber = Int(value * 5)
+        guessLabel.setText("Your guess: \(guessNumber)")
+    }
+    @IBAction func startGuess() {
+        
+        let randomNumber = Int(arc4random_uniform(6))
+        
+        if(guessNumber == randomNumber) {
+            resultLabel.setText("Correct. You win!")
+            
+        } else {
+            resultLabel.setText("Wrong. The number is \(randomNumber)")
+        }
+        
+    }
+    var guessNumber = 3
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
